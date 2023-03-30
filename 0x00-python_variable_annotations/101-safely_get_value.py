3cce956a859683c7b8ad960a4edc1efdc143db9b
M1urray <rknjonjo@gmail.com>
2023-03-30 19:49:38 +0300

    change R to T since TypeVar takes T

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,11 +2,11 @@
 '''More involved type annotations'''
 from typing import Sequence, Union, Any, TypeVar, Mapping
 
-R = TypeVar('T')
+T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[R, None] = None) -> Union[Any, R]:
+                     default: Union[T, None] = None) -> Union[Any, R]:
     ''' Function safely_get_value '''
     if key in dct:
         return dct[key]
